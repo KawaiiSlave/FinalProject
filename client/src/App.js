f01b5d0f1b1e33608eab0594ed5062ee78fd0291
Tyler <tgandle@gmail.com>
2020-04-15 21:59:47 -0500

    make it better

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,25 @@
-import React from 'react';
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route} from "react-router-dom";
-
+import "./App.css"
 import Navbar from "./components/navbar.component"
 import ExpensesList from "./components/expenses-list.component";
 import EditExpense from "./components/edit-expense.component";
 import CreateExpense from "./components/create-expense.component";
 import CreateUser from "./components/create-user.component";
+import React, { useContext } from 'react';
+import { Auth0Context } from './contexts/auth0-context';
 
 function App() {
+  const auth0 = useContext(Auth0Context);
   return (
     <Router>
       <div className="container">
       <Navbar />
       <br/>
+      <div className="container has-text-centered">
+          {auth0.message}
+        </div>
       <Route path="/" exact component={ExpensesList} />
       <Route path="/edit/:id" component={EditExpense} />
       <Route path="/create" component={CreateExpense} />
--- client/src/components/navbar.component.js
+++ client/src/components/navbar.component.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+
+
 export default class Navbar extends Component {
 
   render() {
-    return (
-      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+  return (
+    <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
         <Link to="/" className="navbar-brand">ExpenseTracker</Link>
         <div className="collpase navbar-collapse">
         <ul className="navbar-nav mr-auto">
@@ -21,6 +23,35 @@ export default class Navbar extends Component {
         </ul>
         </div>
       </nav>
-    );
-  }
+  );
 }
+}
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+/*<nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+        <Link to="/" className="navbar-brand">ExpenseTracker</Link>
+        <div className="collpase navbar-collapse">
+        <ul className="navbar-nav mr-auto">
+          <li className="navbar-item">
+          <Link to="/" className="nav-link">Expenses</Link>
+          </li>
+          <li className="navbar-item">
+          <Link to="/create" className="nav-link">Create a new expense</Link>
+          </li>
+          <li className="navbar-item">
+          <Link to="/user" className="nav-link">Create User</Link>
+          </li>
+        </ul>
+        </div>
+      </nav>*/
