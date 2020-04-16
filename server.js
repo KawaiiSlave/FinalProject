ea74d6f0f075ca1bee4ea7c1105f8ab4e4ef8931
Tyler <tgandle@gmail.com>
2020-04-16 16:44:10 -0500

    make it better

--- server.js
+++ server.js
@@ -14,14 +14,6 @@ mongoose.connect(
   { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
 );
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-
-app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
-
 const expensesRouter = require('./routes/expenses');
 const usersRouter = require('./routes/users');
 
